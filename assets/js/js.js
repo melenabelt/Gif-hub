6ee71f7e6b7fc76f604f3df5a81717991669662b
yashaRO <yashanyou@gmail.com>
2016-11-23 05:21:51 -0600

    Beta

---
+++ assets/js/js.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.search').click(function() {
+		$('.gifs').empty()
+		var entry = $('#search').val().trim().match(/\w\s*/gi).join('')
+		var searchTerm = entry.replace(/\s/gi,'+')
+		$('#search').val('')
+		$('.searched').append("<button class='lol' type='button'>" + entry + "</button>")
+		$.ajax({
+			url: 'http://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&api_key=dc6zaTOxFJmzC'
+		}).done(function(data) {
+			console.log(data)
+			var currImg
+			for (i = 0; i < data.data.length; i++) {
+				currImg = data.data[i].images.downsized_medium.url
+				$('.gifs').append("<img class='giffed' src='" + currImg + "'/>")
+			}
+		})
+	})
+
+})
